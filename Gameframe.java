c3233637e672847f978c0b2f2ac99d381cd25306
BandiNiharika <157631930+BandiNiharika@users.noreply.github.com>
2024-07-02 13:07:22 +0530

    Add files via upload

---
+++ Gameframe.java
@@ -0,0 +1,14 @@
+package niharika.com;
+
+import javax.swing.JFrame;
+public class Gameframe extends JFrame{
+Gameframe(){
+this.add(new GamePanel());
+this.setTitle("Snake");
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+this.setResizable(false);
+this.pack();
+this.setVisible(true);;
+this.setLocationRelativeTo(null);
+}
+}
---
+++ Snake.java
@@ -0,0 +1,11 @@
+package niharika.com;
+
+public class Snake {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Gameframe();
+
+	}
+
+}
